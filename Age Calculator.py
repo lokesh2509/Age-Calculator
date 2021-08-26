b872b0ef8891dd51bf0e94206ad0d8e953037f70
Lokesh Vyas <79556675+lokesh1814@users.noreply.github.com>
2021-08-26 11:18:54 +0530

    Update Age Calculator.py

--- Age Calculator.py
+++ Age Calculator.py
@@ -1,17 +1,3 @@
-"""
-Take age or year of birth as an input from the user. Store the input in one variable. Your program should detect whether
-the entered input is age or year of birth and tell the user when they will turn 100 years old. (5 points).
-
-Here are a few instructions that you must have to follow:
-
-Do not use any type of modules like DateTime or date utils. (-5 points)
-Users can optionally provide a year, and your program must tell their age in that particular year. (3points)
-Your code should handle all sort of errors like:                       (2 points)
-You are not yet born
-You seem to be the oldest person alive
-You can also handle any other errors, if possible!
-"""
-
 age = int(input("Enter Your Age or Year of Birth:- "))
 
 if age > 1900:
